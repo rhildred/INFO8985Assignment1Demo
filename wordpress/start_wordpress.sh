8d44ab797fa7abceff7fa445370421c241075728
Richard Hildred <rhildred@gmail.com>
2025-02-06 21:19:00 +0000

    create 0-sqlite.php

--- wordpress/start_wordpress.sh
+++ wordpress/start_wordpress.sh
@@ -4,5 +4,7 @@ if [ ! -d "wp-content/index.php" ]; then
     curl -L https://github.com/WordPress/sqlite-database-integration/archive/refs/heads/develop.zip -o /tmp/sqlite.zip
     mkdir -p wp-content/mu-plugins/sqlite-database-integration
     unzip /tmp/sqlite.zip -d wp-content/mu-plugins/sqlite-database-integration
+    echo '<?php require_once __DIR__ . "/sqlite-database-integration/load.php";' > wp-content/mu-plugins/0-sqlite.php
+    
 fi
 php -S 0.0.0.0:8000
