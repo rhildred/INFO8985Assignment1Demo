953c41a47b3a9b7a5df865badffdad7199113b17
Richard Hildred <rhildred@gmail.com>
2025-02-06 21:15:38 +0000

    start to make local database

--- wordpress/start_wordpress.sh
+++ wordpress/start_wordpress.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-[ ! -d "wp-content/index.php" ] && cp -r wp-content.bak/* wp-content/
+if [ ! -d "wp-content/index.php" ]; then
+    cp -r wp-content.bak/* wp-content/
+    curl -L https://github.com/WordPress/sqlite-database-integration/archive/refs/heads/develop.zip -o /tmp/sqlite.zip
+    mkdir -p wp-content/mu-plugins/sqlite-database-integration
+    unzip /tmp/sqlite.zip -d wp-content/mu-plugins/sqlite-database-integration
+fi
 php -S 0.0.0.0:8000
