6216d7c5fcef6df279f7542267c951279222273c
Richard Hildred <rhildred@gmail.com>
2025-02-14 19:52:59 +0000

    sed the path to be mu-plugins

--- wordpress/start_wordpress.sh
+++ wordpress/start_wordpress.sh
@@ -5,6 +5,7 @@ if [ ! -d "wp-content/index.php" ]; then
     mkdir -p wp-content/mu-plugins
     unzip /tmp/sqlite.zip -d wp-content/mu-plugins
     echo '<?php require_once __DIR__ . "/sqlite-database-integration-develop/load.php";' > wp-content/mu-plugins/0-sqlite.php
-    cp wp-content/mu-plugins/sqlite-database-integration-develop/db.copy wp-content/db.php    
+    cp wp-content/mu-plugins/sqlite-database-integration-develop/db.copy wp-content/db.php
+    sed -i "s/\/plugins\/sqlite-database-integration/\/mu-plugins\/sqlite-database-integration-develop/" wp-content/db.php
 fi
 php -S 0.0.0.0:8000
