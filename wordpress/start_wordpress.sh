435a9ee4dab637f156300a2c18ab10a711b45f9c
Richard Hildred <rhildred@gmail.com>
2025-02-19 19:13:52 +0000

    remove extra space after EOF

--- wordpress/start_wordpress.sh
+++ wordpress/start_wordpress.sh
@@ -5,13 +5,14 @@ if [ ! -d "wp-content/index.php" ]; then
     mkdir -p wp-content/mu-plugins
     unzip /tmp/sqlite.zip -d wp-content/mu-plugins
     echo '<?php require_once __DIR__ . "/sqlite-database-integration-develop/load.php";' > wp-content/mu-plugins/0-sqlite.php
-cat << EOF > wp-content/mu-plugins/0-wp-pwa.php
+    cat << EOF > wp-content/mu-plugins/0-wp-pwa.php
 <?php
     add_filter( 'comments_open', function(){
         return false;
     });
     update_option("permalink_structure","/%postname%/");
-EOF    
+
+EOF
     cp wp-content/mu-plugins/sqlite-database-integration-develop/db.copy wp-content/db.php
     sed -i "s/\/plugins\/sqlite-database-integration/\/mu-plugins\/sqlite-database-integration-develop/" wp-content/db.php
 fi
